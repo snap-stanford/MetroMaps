30e7f1a0ba55c5def2393e4b82307173895c260c
Dima Brezhnev <brezhnd@gmail.com>
2014-01-27 13:54:50 -0800

    switching the converstion to during reading

--- mm/inputhelpers/whitelistcounter.py
+++ mm/inputhelpers/whitelistcounter.py
@@ -1,10 +1,47 @@
+import json
+import utils
 
 class WhiteListCounter():
     def __init__(self, whitelist):
-        pass
-    def toJSON(filename, tokenToID={}):
-        pass
-    def run_filename():
+        self.whitelist = whitelist
+        self.token_to_id = {}
+        self.docname_to_id = {}
+        self.total_counts = {}
+        self.doc_counts = {}
+
+    def toJSON(stream, tokenToID={}, docnameToID={}):
+        # returns four fields : "total_counts", "doc_counts", and "token_to_id" and "docname_to_id"
+
+        next_key_id = max(tokenToID.values())+1 if len(tokenToID) > 0 else 0
+        next_doc_id = max(docnameToID.values()+1 if len(docnameToID) > 0 else 0)
+        def next_key_id():
+            nk = next_key_id
+            next_key_id += 1
+            return nk
+        def next_doc_id():
+            nd = next_doc_id
+            next_doc_id += 1
+
+        out_total_counts = {}
+        for key in self.total_counts:
+            key_id = tokenToID.get(key)
+            if not key_id:
+                key_id = next_key_id()
+                tokenToID[key] = key_id
+
+            current_count = out_total_counts.get(key_id) 
+            out_total_counts[key_id] = current_count + 1 if current_count else 1
+
+        out_doc_counts = {}
+        for key in self.doc_counts:
+
+
+        
+    def run_filename(filename):
+        with open(filename) as fi:
+            for line in fi:
+                for word in line.split():
+
         pass
     def run_dir():
         pass
